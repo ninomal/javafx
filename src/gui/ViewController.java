0f33888a3cc0cc4b97ccfbca09ec4799ba610db9
nino mal <nino_mrt@hotmail.com>
2023-09-05 18:08:33 -0300

    Add sum function

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,18 +1,38 @@
 package gui;
 
+import java.util.Locale;
+
 import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class ViewController {
 	
 	@FXML
-	private Button btTeste;
+	private TextField txtnumberX;
 	
 	@FXML
-	private void onbtTesteAction(){
-		Alerts.showAlert("Error", null, "errou", AlertType.ERROR);
+	private TextField txtnumberY;
+	
+	@FXML
+	private Label labelResult;
+	
+
+	@FXML
+	private Button btSum;
+	
+	@FXML
+	private void onbtSumAction(){
+		Locale.setDefault(Locale.US);
+		Double numberx = Double.parseDouble(txtnumberX.getText());
+		Double numbery = Double.parseDouble(txtnumberY.getText());
+		Double sum = numberx + numbery;
+		labelResult.setText(String.format("%.2f", sum));
+		
 	}
 
 }
