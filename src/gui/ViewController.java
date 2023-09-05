1f74485328b3e326ab9f20d03bacb5a2830d8833
nino mal <nino_mrt@hotmail.com>
2023-09-05 18:40:00 -0300

    Add Contrains and try exce

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,16 +1,19 @@
 package gui;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 import gui.util.Alerts;
+import gui.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 
-public class ViewController {
+public class ViewController implements Initializable {
 	
 	@FXML
 	private TextField txtnumberX;
@@ -27,12 +30,21 @@ public class ViewController {
 	
 	@FXML
 	private void onbtSumAction(){
-		Locale.setDefault(Locale.US);
-		Double numberx = Double.parseDouble(txtnumberX.getText());
-		Double numbery = Double.parseDouble(txtnumberY.getText());
-		Double sum = numberx + numbery;
-		labelResult.setText(String.format("%.2f", sum));
-		
+		try {
+			Locale.setDefault(Locale.US);
+			Double numberx = Double.parseDouble(txtnumberX.getText());
+			Double numbery = Double.parseDouble(txtnumberY.getText());
+			Double sum = numberx + numbery;
+			labelResult.setText(String.format("%.2f", sum));
+		} catch(NumberFormatException e) {
+			Alerts.showAlert("Error", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Constraints.setTextFieldDouble(txtnumberX);
+		Constraints.setTextFieldDouble(txtnumberY);
 	}
 
 }
